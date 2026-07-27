298ad26751a4b1d974ec077c34fa11b910b8ecb0
MVivier69 <michel.vivier@gmail.com>
2026-07-27 09:34:08 +0200

    Modify photo legends in config.js
    Updated photo legends in the gallery configuration.

--- config.js
+++ config.js
@@ -88,9 +88,9 @@ window.USINE_CONFIG = {
     titreSection: "Galerie photos",
     colonnes: 3,
     photos: [
-      { image: "images/galerie/photo-1.jpg", legende: "", lien: "https://www.lusine-belleville.net/photos/visites-de-groupes" },
-      { image: "images/galerie/photo-2.jpg", legende: "", lien: "https://www.lusine-belleville.net/photos/visite-famille-roux" },
-      { image: "images/galerie/photo-3.jpg", legende: "", lien: "https://www.lusine-belleville.net/photos/secrets-de-tonnellerie" }
+      { image: "images/galerie/photo-1.jpg", legende: "Visites de groupes", lien: "https://www.lusine-belleville.net/photos/visites-de-groupes" },
+      { image: "images/galerie/photo-2.jpg", legende: "Visite famille Roux", lien: "https://www.lusine-belleville.net/photos/visite-famille-roux" },
+      { image: "images/galerie/photo-3.jpg", legende: "Secrets de tonnellerie", lien: "https://www.lusine-belleville.net/photos/secrets-de-tonnellerie" }
     ],
     /* Bouton facultatif sous la galerie ("" = pas de bouton) */
     boutonTexte: "",
